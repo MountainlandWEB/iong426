d6665d5c5bd28f5a7260a1a3c085fdea56a8cac9
Todd H <github@simplybest.com>
2026-04-28 20:15:49 -0600

    add pwa, add speech recog

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,52 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Component, OnInit, signal } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonTextarea } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { stopCircleOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonTextarea, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon],
 })
-export class Tab1Page {
-  constructor() {}
+export class Tab1Page implements OnInit {
+  recognition!: any;
+  recognizedText = signal<string>('');
+  isListening = signal<boolean>(false);
+
+  constructor() {
+    addIcons({ stopCircleOutline });
+    const SpeechRecognition = (window as any).SpeechRecognition ||
+                              (window as any).webkitSpeechRecognition;
+    if (!SpeechRecognition) {
+      console.error('Speech Recognition not supported');
+      return;
+    }
+    this.recognition = new SpeechRecognition();
+  }
+
+  ngOnInit() {
+    this.recognition.continuous = false;
+    this.recognition.lang = 'en-US';
+    this.recognition.interimResults = false;
+    this.recognition.maxAlternatives = 1;
+
+    this.recognition.onstart = () => this.isListening.set(true);
+    this.recognition.onend = () => this.isListening.set(false);
+
+    this.recognition.onresult = (event: any) => {
+      const transcript = event.results[event.results.length - 1][0].transcript;
+      this.recognizedText.set(transcript);
+      console.log('Recognized text:', transcript);
+    };
+
+    this.recognition.onerror = (event: any) => {
+      console.error('Speech recognition error:', event.error);
+    };
+  }
+
+  record() {
+    this.recognition.start();
+  }
+
 }
